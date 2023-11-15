66940d2a999dd6f0114774f10097e8754552e722
Sandro Chabukiani <119055272+SandroChabukiani@users.noreply.github.com>
2023-11-15 14:05:07 +0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+
+const TodoApp = () => {
+  const [todos, setTodos] = useState([]);
+  const [task, setTask] = useState('');
+
+  const addTodo = () => {
+    if (task.trim() !== '') {
+      setTodos([...todos, { id: Date.now(), text: task, completed: false }]);
+      setTask('');
+    }
+  };
+
+  const toggleTodo = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+
+  const deleteTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  return (
+    <div>
+      <h1>Todo App</h1>
+      <div>
+        <input
+          type="text"
+          value={task}
+          onChange={(e) => setTask(e.target.value)}
+        />
+        <button onClick={addTodo}>Add Todo</button>
+      </div>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            <input
+              type="checkbox"
+              checked={todo.completed}
+              onChange={() => toggleTodo(todo.id)}
+            />
+            <span style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}>
+              {todo.text}
+            </span>
+            <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default TodoApp;
